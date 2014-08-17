b56155e54bcd9ab5af78802e5887ea0c7b72e602
gitPoc32 <gitPoc32@users.noreply.github.com>
2014-08-17 19:49:15 +0000

    Update configg.php

--- master/Dendroid Panel/Dendroid Panel/Panel/configg.php
+++ master/Dendroid Panel/Dendroid Panel/Panel/configg.php
@@ -2,4 +2,6 @@
 // define url of control panel and domain
 $url = "http://pizzachip.com/rat/";
 $allowedDomains = array("www.pizzachip.com", "pizzachip.com");
+// define the password of the access
+$passwords = "Password";
 ?>
